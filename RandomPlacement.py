4c12b7ab4a2a372b6bc635f0c153e0379a94ea3a
Brandon <brandonjorgen@gmail.com>
2020-11-21 21:22:21 -0700

    Rename Objects Window created, progress on Random Placement

--- RandomPlacement.py
+++ RandomPlacement.py
@@ -2,14 +2,55 @@ import maya.cmds as cmds
 import random
 
 
-def randomDuplication(dupeAmount, minX, maxX, minY, maxY, minZ, maxZ):
-    selection = cmds.ls(selection=True)
-    objs = []
+class RandomPlacement():
+    def __init__(self):
+        self.created_window = "bjWindow"
 
-    for obj in selection:
-        for dupeObj in range(0, dupeAmount):
-            objs = cmds.duplicate(obj)
-            cmds.xform(objs, ws=True, t=(random.uniform(minX, maxX), random.uniform(minY, maxY), random.uniform(minZ, maxZ)))
+    def randomDuplication(self, dupeAmount, minX, maxX, minY, maxY, minZ, maxZ):
+        selection = cmds.ls(selection=True)
+        objs = []
+        dupeAmount = cmds.intSlider(self.dupeAmountSlider, q=True, value=True)
+        minX = cmds.intSlider(self.minXSlider, q=True, value=True)
+        maxX = cmds.intSlider(self.maxXSlider, q=True, value=True)
+        minY = cmds.intSlider(self.minYSlider, q=True, value=True)
+        maxY = cmds.intSlider(self.maxYSlider, q=True, value=True)
+        minZ = cmds.intSlider(self.minZSlider, q=True, value=True)
+        maxZ = cmds.intSlider(self.maxZSlider, q=True, value=True)
+
+        for obj in selection:
+            for dupeObj in range(0, dupeAmount):
+                objs = cmds.duplicate(obj)
+                cmds.xform(objs, ws=True, t=(random.uniform(minX, maxX), random.uniform(minY, maxY), random.uniform(minZ, maxZ)))
 
 
-randomDuplication(1, 0, 12, 0, 12, 0, 12)
+    def CreateWindow(self):
+        self.DeleteWindow()
+
+        self.window = cmds.window(self.created_window, title="Random Placement Window", widthHeight=(400, 200))
+        self.column = cmds.rowColumnLayout(parent=self.window, numberOfColumns=3)
+        cmds.text(parent=self.column, label="Duplication amount: ")
+        self.dupeAmountSlider = cmds.intSlider(min=1, max=100, value=1, step=1)
+        cmds.text(parent=self.column, label="Min X: ")
+        self.minXSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.minXValue = cmds.text(parent=self.column, label="value")
+        cmds.text(parent=self.column, label="Max X: ")
+        self.maxXSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.maxXValue = cmds.text(parent=self.column, label="value")
+        cmds.text(parent=self.column, label="Min Y: ")
+        self.minYSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.minYValue = cmds.text(parent=self.column, label="value")
+        cmds.text(parent=self.column, label="Max Y: ")
+        self.maxYSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.maxYValue = cmds.text(parent=self.column, label="value")
+        cmds.text(parent=self.column, label="Min Z: ")
+        self.minZSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.minZValue = cmds.text(parent=self.column, label="value")
+        cmds.text(parent=self.column, label="Max Z: ")
+        self.maxZSlider = cmds.intSlider(min=0, max=12, value=0, step=1)
+        self.maxZValue = cmds.text(parent=self.column, label="value")
+        cmds.button(parent=self.column, label="Execute", c=self.randomDuplication)
+        cmds.showWindow(self.window)
+
+    def DeleteWindow(self):
+        if cmds.window(self.created_window, exists=True):
+            cmds.deleteUI(self.created_window)
--- RenameObjects.py
+++ RenameObjects.py
@@ -1,19 +1,38 @@
 import maya.cmds as cmds
 
 
-def RenameObject(name_format):
-    selection = cmds.ls(selection=True)
-    current_sel_count = 1
-    for obj in selection:
-        num_of_hash = name_format.count("#")
-        if num_of_hash > 0:
-            obj_partition = name_format.partition("#" * num_of_hash)
-            sequence_string = str(current_sel_count)
-            cmds.select(obj)
-            sequence_string = sequence_string.zfill(num_of_hash)
-            cmds.rename(obj_partition[0] + sequence_string + obj_partition[2])
-            cmds.rename(obj_partition[0] + sequence_string + obj_partition[2])
-            current_sel_count += 1
+class Renamer():
+    def __init__(self):
+        self.created_window = "bjWindow"
 
+    def RenameObject(self, name_format):
+        selection = cmds.ls(selection=True)
+        name_format = cmds.textField(self.textInput, q=True, text=True)
+        current_sel_count = 1
+        for obj in selection:
+            num_of_hash = name_format.count("#")
+            if num_of_hash > 0:
+                obj_partition = name_format.partition("#" * num_of_hash)
+                sequence_string = str(current_sel_count)
+                cmds.select(obj)
+                sequence_string = sequence_string.zfill(num_of_hash)
+                cmds.rename(obj_partition[0] + sequence_string + obj_partition[2])
+                cmds.rename(obj_partition[0] + sequence_string + obj_partition[2])
+                current_sel_count += 1
+            else:
+                cmds.error("No # symbols located in text input")
 
-RenameObject("Leg_##_Jnt")
+    def CreateWindow(self):
+        self.DeleteWindow()
+
+        self.window = cmds.window(self.created_window, title="Object Renamer Window", widthHeight=(400, 200))
+        self.column = cmds.rowColumnLayout(parent=self.window, numberOfColumns=2)
+        cmds.text(parent=self.column, label="Text Input Field: ")
+        self.textInput = cmds.textField(parent=self.column)
+        cmds.button(parent=self.column, label="Execute", c=self.RenameObject)
+        cmds.showWindow(self.window)
+
+    def DeleteWindow(self):
+        if cmds.window(self.created_window, exists=True):
+            cmds.deleteUI(self.created_window)
+
